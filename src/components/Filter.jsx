956f38ac36c0796eb2ad90dda5b1375586f09a9e
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-02 12:34:46 -0300

    :sparkles: feat: Adiciona componentes Filter e Search

---
+++ src/components/Filter.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+// eslint-disable-next-line react/prop-types, no-unused-vars
+const Filter = ({ filter, setFilter, setSort }) => {
+  return (
+    <div className="filter">
+      <h2>Filtrar:</h2>
+      <div className="filter-options">
+        <div>
+          <p>Status:</p>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="All">Todas</option>
+            <option value="Completed">Concluidas</option>
+            <option value="Incomplete">Incompletas</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
