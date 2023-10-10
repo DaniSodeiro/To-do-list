29a402b855480baad6c10982c4f46707e73ebf42
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-10 15:29:36 -0300

    :bulb: docs:  Adiciona comentário na main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from "react"; // Importa a biblioteca React
+import ReactDOM from "react-dom/client"; // Importa o ReactDOM para renderizar o aplicativo
+import App from "./App.jsx"; // Importa o componente principal do aplicativo
 
+// Renderiza o aplicativo dentro de um elemento com a ID "root" usando ReactDOM.createRoot
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {" "}
+    {/* Modo estrito do React */}
+    <App /> {/* Renderiza o componente App (o ponto de entrada da aplicação) */}
   </React.StrictMode>
 );
