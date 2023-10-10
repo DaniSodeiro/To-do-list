0f1212ae1cb679332c7b62da187a588aad7f9e97
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-10 15:29:54 -0300

    :bulb: docs:  Adiciona comentário no Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
+// Declaração do componente Filter, que recebe três propriedades como argumentos.
 const Filter = ({ filter, setFilter, setSort }) => {
   return (
+    // Renderiza uma div com a classe CSS "filter".
     <div className="filter">
+      {/* Título "Filtrar:" */}
       <h2>Filtrar:</h2>
+      {/* Div para as opções de filtro */}
       <div className="filter-options">
         <div>
+          {/* Parágrafo com o texto "Status:" */}
           <p>Status:</p>
+          {/* Dropdown (select) para escolher o filtro com valor controlado por 'filter' */}
           <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            {/* Opções do dropdown */}
             <option value="All">Todas</option>
-            <option value="Completed">Concluidas</option>
+            <option value="Completed">Concluídas</option>
             <option value="Incomplete">Pendentes</option>
           </select>
         </div>
@@ -18,4 +25,5 @@ const Filter = ({ filter, setFilter, setSort }) => {
   );
 };
 
+// Exporta o componente Filter para ser usado em outros lugares.
 export default Filter;
