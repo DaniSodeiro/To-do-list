70c217db45a2d6bba0cd6f025e6aa1e893de8e6d
DaniSodeiro <danielasodeirosilva@gmail.com>
2023-10-10 15:30:28 -0300

    :bulb: docs:  Adiciona comentário no Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 
+// Declaração do componente Todo, que recebe quatro propriedades como argumentos.
 const Todo = ({ todo, completeTodo, editTodo, removeTodo }) => {
+  // Definição locais para controle da edição do todo.
   const [isEditing, setIsEditing] = useState(false);
   const [updatedText, setUpdatedText] = useState(todo.text);
 
+  // Função para iniciar a edição do todo.
   const handleEdit = () => {
     setIsEditing(true);
   };
 
+  // Função para salvar as alterações no todo.
   const handleSave = () => {
     editTodo(todo.id, updatedText);
     setIsEditing(false);
   };
 
+  // Função para atualizar o texto do todo enquanto estiver editando.
   const handleTextChange = (e) => {
     setUpdatedText(e.target.value);
   };
@@ -24,14 +29,18 @@ const Todo = ({ todo, completeTodo, editTodo, removeTodo }) => {
     >
       <div className="content">
         {isEditing ? (
+          // Renderiza um input de texto durante a edição.
           <input type="text" value={updatedText} onChange={handleTextChange} />
         ) : (
+          // Renderiza o texto do todo.
           <p>{todo.text}</p>
         )}
+        {/* Exibe a categoria do todo entre parênteses. */}
         <p className="category">({todo.category})</p>
       </div>
       <div>
         {isEditing ? (
+          // Renderiza botões de "Salvar" e "Cancelar" durante a edição.
           <>
             <button className="save" onClick={handleSave}>
               Salvar
@@ -41,9 +50,10 @@ const Todo = ({ todo, completeTodo, editTodo, removeTodo }) => {
             </button>
           </>
         ) : (
+          // Renderiza botões de "Concluir", "Editar" e "Excluir" quando não estiver editando.
           <>
             <button className="complete" onClick={() => completeTodo(todo.id)}>
-              Concluido
+              Concluído
             </button>
             <button className="edit" onClick={handleEdit}>
               Editar
@@ -58,4 +68,5 @@ const Todo = ({ todo, completeTodo, editTodo, removeTodo }) => {
   );
 };
 
+// Exporta o componente Todo para ser usado em outros lugares.
 export default Todo;
